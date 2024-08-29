5fa71af9a57f77be6be9453c85b562cd6541ed09
BorysLytochkin <borisleen2019@gmail.com>
2024-08-29 12:57:16 +0300

    new html and js

---
+++ Ex1/app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function addThemAll(...args) {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum;
+}
+
+console.log(addThemAll(2, 4)); // 6
+console.log(addThemAll(1, 2, 3, 4)); // 10
+console.log(addThemAll(5, 5, 10)); // 20
